43847e7c9b71642c683b417194c58a7ecefba8a6
zack09holland <zack09holland@gmail.com>
2019-08-03 21:26:35 -0700

    added some of the requirement variables

--- liri.js
+++ liri.js
@@ -1,6 +1,10 @@
+const dotenv = require("dotenv").config();
+var keys = require("./keys.js");
+var spotifyKey = new spotifyKey(keys.spotify);
+var omdbKey = keys.omdb.api_key;gffff
 var axios = require('axios');
 
-var movieName = process.argv[2];
+//var movieName = process.argv[2];
 
 var url = `http://www.omdbapi.com/?t=${ movieName }&y=&plot=short&apikey=trilogy`;
 
